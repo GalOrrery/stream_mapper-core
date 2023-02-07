65df5f5cf2d24d51821a93c18f8146546356223a
nstarman <nstarkman@protonmail.com>
2023-02-07 12:50:22 -0500

    add end perens
    Signed-off-by: nstarman <nstarkman@protonmail.com>

--- src/stream_ml/core/data.py
+++ src/stream_ml/core/data.py
@@ -117,8 +117,11 @@ class Data(Generic[Array]):
     def __str__(self) -> str:
         """Get the string representation."""
         array = indent(repr(self.array), prefix="\t")[1:]
-        return "\t".join(
-            (f"{type(self).__name__}(", f"names: {self.names!r}", f"array: {array}")
+        return (
+            "\t".join(
+                (f"{type(self).__name__}(", f"names: {self.names!r}", f"array: {array}")
+            )
+            + ")"
         )
 
     # -----------------------------------------------------------------------
