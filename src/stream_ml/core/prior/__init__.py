49ce8ba43a1f3f6cbd01c285dd55b596b7f8530e
nstarman <nathanielstarkman@gmail.com>
2023-03-28 22:54:32 -0400

    import ClippedBounds

--- src/stream_ml/core/prior/__init__.py
+++ src/stream_ml/core/prior/__init__.py
@@ -1,8 +1,15 @@
 """Core library for stream membership likelihood, with ML."""
 
 from stream_ml.core.prior.base import PriorBase
-from stream_ml.core.prior.bounds import NoBounds, PriorBounds
+from stream_ml.core.prior.bounds import ClippedBounds, NoBounds, PriorBounds
 from stream_ml.core.prior.core import Prior
 from stream_ml.core.prior.weight import HardThreshold
 
-__all__ = ["PriorBase", "Prior", "PriorBounds", "NoBounds", "HardThreshold"]
+__all__ = [
+    "PriorBase",
+    "Prior",
+    "PriorBounds",
+    "NoBounds",
+    "ClippedBounds",
+    "HardThreshold",
+]
