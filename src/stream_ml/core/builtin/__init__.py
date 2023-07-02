006fdecfc1d5698c877b1de3ea1b926328ffacd0
Nathaniel Starkman <nstarman@users.noreply.github.com>
2023-07-02 17:44:39 -0400

    import builtins (#134)
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/stream_ml/core/builtin/__init__.py
+++ src/stream_ml/core/builtin/__init__.py
@@ -1,7 +1,21 @@
-"""Background models."""
+"""Built-in Models."""
 
 
+from stream_ml.core.builtin._exponential import Exponential
 from stream_ml.core.builtin._norm import Normal
+from stream_ml.core.builtin._skewnorm import SkewNormal
+from stream_ml.core.builtin._truncnorm import TruncatedNormal
+from stream_ml.core.builtin._truncskewnorm import TruncatedSkewNormal
 from stream_ml.core.builtin._uniform import Uniform
+from stream_ml.core.builtin._utils import WhereRequiredError
 
-__all__ = ["Uniform", "Normal"]
+__all__ = [
+    "Uniform",
+    "Exponential",
+    "Normal",
+    "TruncatedNormal",
+    "SkewNormal",
+    "TruncatedSkewNormal",
+    # ---
+    "WhereRequiredError",
+]
