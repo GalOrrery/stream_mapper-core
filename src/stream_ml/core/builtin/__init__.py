3212290dbb0461a2196a3ec86512e79054d83fce
nstarman <nstarman@users.noreply.github.com>
2023-10-11 16:32:57 -0400

    Add Isochrone stuff
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/stream_ml/core/builtin/__init__.py
+++ src/stream_ml/core/builtin/__init__.py
@@ -1,14 +1,5 @@
 """Built-in models."""
 
-
-from stream_ml.core.builtin._exponential import Exponential
-from stream_ml.core.builtin._norm import Normal
-from stream_ml.core.builtin._skewnorm import SkewNormal
-from stream_ml.core.builtin._truncnorm import TruncatedNormal
-from stream_ml.core.builtin._truncskewnorm import TruncatedSkewNormal
-from stream_ml.core.builtin._uniform import Uniform
-from stream_ml.core.builtin._utils import WhereRequiredError
-
 __all__ = [
     "Uniform",
     "Exponential",
@@ -18,4 +9,26 @@ __all__ = [
     "TruncatedSkewNormal",
     # ---
     "WhereRequiredError",
+    # -- isochrone
+    "StreamMassFunction",
+    "UniformStreamMassFunction",
+    "HardCutoffMassFunction",
+    "StepwiseMassFunction",
+    "Parallax2DistMod",
 ]
+
+
+from stream_ml.core.builtin._exponential import Exponential
+from stream_ml.core.builtin._isochrone import (
+    HardCutoffMassFunction,
+    Parallax2DistMod,
+    StepwiseMassFunction,
+    StreamMassFunction,
+    UniformStreamMassFunction,
+)
+from stream_ml.core.builtin._norm import Normal
+from stream_ml.core.builtin._skewnorm import SkewNormal
+from stream_ml.core.builtin._truncnorm import TruncatedNormal
+from stream_ml.core.builtin._truncskewnorm import TruncatedSkewNormal
+from stream_ml.core.builtin._uniform import Uniform
+from stream_ml.core.builtin._utils import WhereRequiredError
