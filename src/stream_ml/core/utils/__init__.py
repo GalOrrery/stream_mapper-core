16c5e56642d582c37ec8656f5d57bfbe52f5e6af
nstarman <nstarman@users.noreply.github.com>
2023-10-13 16:36:31 -0400

    cleanup utils init
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/stream_ml/core/utils/__init__.py
+++ src/stream_ml/core/utils/__init__.py
@@ -1,16 +1,25 @@
 """Utilities."""
 
-from stream_ml.core.utils.compat import array_at, get_namespace
-from stream_ml.core.utils.funcs import within_bounds
-from stream_ml.core.utils.scale._api import DataScaler
-from stream_ml.core.utils.scale._multi import CompoundDataScaler
-from stream_ml.core.utils.scale._standard import StandardScaler
-
 __all__ = [
+    # compat
     "array_at",
     "get_namespace",
+    "copy",
+    # funcs
     "within_bounds",
-    "DataScaler",
+    "pairwise_distance",
+    # scale
     "CompoundDataScaler",
     "StandardScaler",
+    "DataScaler",
+    "names_intersect",
 ]
+
+from stream_ml.core.utils.compat import array_at, copy, get_namespace
+from stream_ml.core.utils.funcs import pairwise_distance, within_bounds
+from stream_ml.core.utils.scale import (
+    CompoundDataScaler,
+    DataScaler,
+    StandardScaler,
+    names_intersect,
+)
